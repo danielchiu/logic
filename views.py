22d779296daf45d600220a81c5705b3842bcc700
dchiu <tuxianeer@gmail.com>
2016-07-13 23:34:34 -0700

    made initial stuff, stylesheet broken

---
+++ views.py
@@ -0,0 +1,25 @@
+from flask import * # TODO actually look at imports
+
+views = Blueprint("views",__name__)
+
+@views.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@views.route("/login", methods = ["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form["username"] != "user": # temp username and password for now
+            error = "Invalid username"
+        elif request.form["password"] != "pass":
+            error = "Invalid password"
+        else:
+            session["logged_in"] = True
+            return redirect("/")
+    return render_template("login.html",error = error)
+
+@views.route("/logout")
+def logout():
+    session.pop("logged_in", None)
+    return redirect("/")
