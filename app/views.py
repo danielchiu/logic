6653e9e2bceef6d55780c5ea7f5edeb7ff308160
dchiu <tuxianeer@gmail.com>
2016-09-20 13:50:48 -0700

    you can login now!

--- app/views.py
+++ app/views.py
@@ -7,24 +7,33 @@ views = Blueprint("views",__name__)
 
 @views.route("/")
 def homepage():
-    return render_template("homepage.html")
+    user = None
+    if "user" in session:
+        user = session["user"]
+    return render_template("homepage.html",user=user)
 
 @views.route("/login", methods = ["GET", "POST"])
 def login():
     error = None
     if request.method == "POST":
-        if request.form["username"] != "user": # temp username and password for now
+        '''if request.form["username"] != "user": # temp username and password for now
             error = "Invalid username"
         elif request.form["password"] != "pass":
             error = "Invalid password"
         else:
             session["logged_in"] = True
+            return redirect("/")'''
+        user = User.query.filter_by(username=request.form["username"]).first()
+        if user is None:
+            error = "No such user exists"
+        else:
+            session["user"] = user.username # TODO is there a way to make it whole User class
             return redirect("/")
     return render_template("login.html",error = error)
 
 @views.route("/logout")
 def logout():
-    session.pop("logged_in", None)
+    session.pop("user", None)
     return redirect("/")
 
 @views.route("/register", methods = ["GET", "POST"])
