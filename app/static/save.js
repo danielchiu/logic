eb15716699785dcf2ea6fd4db69ceb16320eb186
dchiu <tuxianeer@gmail.com>
2016-12-05 21:37:04 -0800

    fixed issues with whitespace in notes

--- app/static/save.js
+++ app/static/save.js
@@ -1,5 +1,5 @@
 $("#content").on("click", "#save", function() {
     var request = new XMLHttpRequest();
-    request.open("POST", "?type=savenote&note="+$("#notes .box").val());
+    request.open("POST", "?type=savenote&note="+$("#notes .box").val().replace(/\n/g,"%0A"));
     request.send();
 });
